9204fcce0b2674d075946877928f597b783ed136
First name hg <cistakovvasilijaleksandrovic@MacBook-Pro-Cistakov.local>
2021-05-12 18:30:36 +0300

    1

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mapProject
+//
+//  Created by Чистяков Василий Александрович on 12.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
